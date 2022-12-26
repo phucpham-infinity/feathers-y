d95f19998eb43e2335c4b0de477e0e7502d4eaad
phuc.pham <phucpham.infinity@gmail.com>
2022-12-26 08:27:23 +0700

    first commit

---
+++ lib/services/users/users.schema.d.ts
@@ -0,0 +1,249 @@
+import type { Static } from '@feathersjs/typebox';
+import type { HookContext } from '../../declarations';
+export declare const userSchema: import("@sinclair/typebox").TObject<{
+    _id: import("@sinclair/typebox").TString<string>;
+    email: import("@sinclair/typebox").TString<string>;
+    password: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+    auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+}>;
+export type User = Static<typeof userSchema>;
+export declare const userResolver: import("@feathersjs/schema").Resolver<{
+    password?: string | undefined;
+    auth0Id?: string | undefined;
+    _id: string;
+    email: string;
+}, HookContext<any>>;
+export declare const userExternalResolver: import("@feathersjs/schema").Resolver<{
+    password?: string | undefined;
+    auth0Id?: string | undefined;
+    _id: string;
+    email: string;
+}, HookContext<any>>;
+export declare const userDataSchema: import("@sinclair/typebox").TPick<import("@sinclair/typebox").TObject<{
+    _id: import("@sinclair/typebox").TString<string>;
+    email: import("@sinclair/typebox").TString<string>;
+    password: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+    auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+}>, ["email", "password", "auth0Id"]>;
+export type UserData = Static<typeof userDataSchema>;
+export declare const userDataValidator: import("@feathersjs/schema").DataValidatorMap;
+export declare const userDataResolver: import("@feathersjs/schema").Resolver<{
+    password?: string | undefined;
+    auth0Id?: string | undefined;
+    _id: string;
+    email: string;
+}, HookContext<any>>;
+export declare const userPatchSchema: import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+    _id: import("@sinclair/typebox").TString<string>;
+    email: import("@sinclair/typebox").TString<string>;
+    password: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+    auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+}>>;
+export type UserPatch = Static<typeof userPatchSchema>;
+export declare const userPatchValidator: import("@feathersjs/schema").DataValidatorMap;
+export declare const userPatchResolver: import("@feathersjs/schema").Resolver<{
+    password?: string | undefined;
+    auth0Id?: string | undefined;
+    _id: string;
+    email: string;
+}, HookContext<any>>;
+export declare const userQueryProperties: import("@sinclair/typebox").TPick<import("@sinclair/typebox").TObject<{
+    _id: import("@sinclair/typebox").TString<string>;
+    email: import("@sinclair/typebox").TString<string>;
+    password: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+    auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+}>, ["_id", "email", "auth0Id"]>;
+export declare const userQuerySchema: import("@sinclair/typebox").TIntersect<[import("@sinclair/typebox").TIntersect<[import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+    $limit: import("@sinclair/typebox").TNumber;
+    $skip: import("@sinclair/typebox").TNumber;
+    $sort: import("@sinclair/typebox").TObject<{
+        _id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TInteger>;
+        email: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TInteger>;
+        auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TInteger>;
+    }>;
+    $select: import("@sinclair/typebox").TUnsafe<("_id" | "email" | "auth0Id")[]>;
+    $or: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TObject<{
+        _id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TString<string>;
+            $gte: import("@sinclair/typebox").TString<string>;
+            $lt: import("@sinclair/typebox").TString<string>;
+            $lte: import("@sinclair/typebox").TString<string>;
+            $ne: import("@sinclair/typebox").TString<string>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        }>>]>>;
+        email: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TString<string>;
+            $gte: import("@sinclair/typebox").TString<string>;
+            $lt: import("@sinclair/typebox").TString<string>;
+            $lte: import("@sinclair/typebox").TString<string>;
+            $ne: import("@sinclair/typebox").TString<string>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        }>>]>>;
+        auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $gte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $lt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $lte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $ne: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+        }>>]>>;
+    }>>>;
+    $and: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TObject<{
+        _id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TString<string>;
+            $gte: import("@sinclair/typebox").TString<string>;
+            $lt: import("@sinclair/typebox").TString<string>;
+            $lte: import("@sinclair/typebox").TString<string>;
+            $ne: import("@sinclair/typebox").TString<string>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        }>>]>>;
+        email: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TString<string>;
+            $gte: import("@sinclair/typebox").TString<string>;
+            $lt: import("@sinclair/typebox").TString<string>;
+            $lte: import("@sinclair/typebox").TString<string>;
+            $ne: import("@sinclair/typebox").TString<string>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        }>>]>>;
+        auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+            $gt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $gte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $lt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $lte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $ne: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+            $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+            $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+        }>>]>>;
+    }>>>;
+}>>, import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TObject<{
+    _id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+        $gt: import("@sinclair/typebox").TString<string>;
+        $gte: import("@sinclair/typebox").TString<string>;
+        $lt: import("@sinclair/typebox").TString<string>;
+        $lte: import("@sinclair/typebox").TString<string>;
+        $ne: import("@sinclair/typebox").TString<string>;
+        $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+    }>>]>>;
+    email: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TString<string>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+        $gt: import("@sinclair/typebox").TString<string>;
+        $gte: import("@sinclair/typebox").TString<string>;
+        $lt: import("@sinclair/typebox").TString<string>;
+        $lte: import("@sinclair/typebox").TString<string>;
+        $ne: import("@sinclair/typebox").TString<string>;
+        $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+        $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TString<string>>;
+    }>>]>>;
+    auth0Id: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TUnion<[import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>, import("@sinclair/typebox").TPartial<import("@sinclair/typebox").TObject<{
+        $gt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+        $gte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+        $lt: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+        $lte: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+        $ne: import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>;
+        $in: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+        $nin: import("@sinclair/typebox").TArray<import("@sinclair/typebox").TOptional<import("@sinclair/typebox").TString<string>>>;
+    }>>]>>;
+}>>]>, import("@sinclair/typebox").TObject<{}>]>;
+export type UserQuery = Static<typeof userQuerySchema>;
+export declare const userQueryValidator: import("@feathersjs/schema").Validator<any, any>;
+export declare const userQueryResolver: import("@feathersjs/schema").Resolver<Partial<{
+    $limit: number;
+    $skip: number;
+    $sort: {
+        _id?: number | undefined;
+        email?: number | undefined;
+        auth0Id?: number | undefined;
+    };
+    $select: ("_id" | "email" | "auth0Id")[];
+    $or: {
+        _id?: string | Partial<{
+            $gt: string;
+            $gte: string;
+            $lt: string;
+            $lte: string;
+            $ne: string;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+        email?: string | Partial<{
+            $gt: string;
+            $gte: string;
+            $lt: string;
+            $lte: string;
+            $ne: string;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+        auth0Id?: string | Partial<{
+            $gt?: string | undefined;
+            $gte?: string | undefined;
+            $lt?: string | undefined;
+            $lte?: string | undefined;
+            $ne?: string | undefined;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+    }[];
+    $and: {
+        _id?: string | Partial<{
+            $gt: string;
+            $gte: string;
+            $lt: string;
+            $lte: string;
+            $ne: string;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+        email?: string | Partial<{
+            $gt: string;
+            $gte: string;
+            $lt: string;
+            $lte: string;
+            $ne: string;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+        auth0Id?: string | Partial<{
+            $gt?: string | undefined;
+            $gte?: string | undefined;
+            $lt?: string | undefined;
+            $lte?: string | undefined;
+            $ne?: string | undefined;
+            $in: string[];
+            $nin: string[];
+        }> | undefined;
+    }[];
+}> & {
+    _id?: string | Partial<{
+        $gt: string;
+        $gte: string;
+        $lt: string;
+        $lte: string;
+        $ne: string;
+        $in: string[];
+        $nin: string[];
+    }> | undefined;
+    email?: string | Partial<{
+        $gt: string;
+        $gte: string;
+        $lt: string;
+        $lte: string;
+        $ne: string;
+        $in: string[];
+        $nin: string[];
+    }> | undefined;
+    auth0Id?: string | Partial<{
+        $gt?: string | undefined;
+        $gte?: string | undefined;
+        $lt?: string | undefined;
+        $lte?: string | undefined;
+        $ne?: string | undefined;
+        $in: string[];
+        $nin: string[];
+    }> | undefined;
+} & {}, HookContext<any>>;
